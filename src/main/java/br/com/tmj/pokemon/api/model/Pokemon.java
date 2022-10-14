a6d634e3e97b13dc4e284db392caca7b6a6dd011
Wellington Defassio <well.defassio@gmail.com>
2022-10-13 22:00:01 -0300

    Initial commit: pokemon project

---
+++ src/main/java/br/com/tmj/pokemon/api/model/Pokemon.java
@@ -0,0 +1,2 @@
+package br.com.tmj.pokemon.api.model;public class Pokemon {
+}
---
+++ src/test/java/br/com/tmj/pokemon/api/PokemonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tmj.pokemon.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
